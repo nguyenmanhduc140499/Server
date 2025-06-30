a4166d945f6069fd64f193715ea0a7899ff5ce80
nguyen manh duc <nguyenmanhduc406@gmail.com>
2025-06-30 17:59:24 +0700

    update index file

--- src/Resolver/index.ts
+++ src/Resolver/index.ts
@@ -1,7 +1,6 @@
-import { CollectionService } from "../Service/collection.service";
 import CollectionResolver from "./collection.resolver";
 import ProductResolver from "./product.resolver";
 import UserResolver from "./user.resolver";
 import OrderResolver from "./order.resolver";
 
-export const resolvers = [ProductResolver, CollectionResolver, CollectionService, UserResolver, OrderResolver] as const;
+export const resolvers = [ProductResolver, CollectionResolver, UserResolver, OrderResolver] as const;
